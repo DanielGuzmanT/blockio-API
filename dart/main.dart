4f19244f6601637970a3f791ae26633cce6df90d
DanielGuzmanT <jhairguz.t@gmail.com>
2019-03-23 23:28:57 -0500

    print block color salmon in browser

--- dart/main.dart
+++ dart/main.dart
@@ -1,9 +1,27 @@
 import 'dart:html';
+import 'dart:math';
 
 CanvasElement canvas;
 CanvasRenderingContext2D context;
 
+const int CELL_SIZE = 10;
+
+
 void main() {
   canvas = querySelector("#canvas");
   context = canvas.getContext("2d");
+
+  drawCell(new Point(10, 10), "salmon");
+}
+
+
+void drawCell(Point coords, String color) {
+  context..fillStyle = color
+  ..strokeStyle = "white";
+
+  final int x = coords.x * CELL_SIZE;
+  final int y = coords.y * CELL_SIZE;
+  
+  context..fillRect(x, y, CELL_SIZE, CELL_SIZE)
+  ..strokeRect(x, y, CELL_SIZE, CELL_SIZE);
 }
